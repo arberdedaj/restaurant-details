51dd612925917d2f39d8323e4261cff8447e88d8
Arber Dedaj <>
2021-12-20 20:36:41 +0100

    project setup

---
+++ RestaurantDetails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantDetails
+//
+//  Created by Arber Dedaj on 20.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
