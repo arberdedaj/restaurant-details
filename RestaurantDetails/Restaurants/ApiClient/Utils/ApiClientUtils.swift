a0f8750b1a3dc8bc29ed6b2609555661d536159c
Arber Dedaj <>
2021-12-26 09:31:07 +0100

    Refactoring

--- RestaurantDetails/Restaurants/ApiClient/Utils/ApiClientUtils.swift
+++ RestaurantDetails/Restaurants/ApiClient/Utils/ApiClientUtils.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class ApiClientUtils {
+
+    static func createUrlRequest(baseUrl: String,
+                                 path: String,
+                                 headers: [String: String],
+                                 queryParams: [String: String]) throws -> URLRequest {
+        guard var components = URLComponents(string: baseUrl) else {
+            let error = NSError(domain: Bundle.main.bundleIdentifier ?? "",
+                                code: 0,
+                                userInfo: [NSLocalizedDescriptionKey: "Could not create url request",
+                                    NSLocalizedFailureReasonErrorKey: "The provided baseUrl is not valid"])
+            throw error
+        }
+        components.path = components.path.appending(path)
+        components.queryItems = queryParams.compactMap { URLQueryItem(name: $0.key,
+                                                                      value: $0.value) }
+        
+        if let url = components.url {
+            var request  = URLRequest(url: url)
+            request.httpMethod = "GET"
+            headers.forEach { request.setValue($0.value,
+                                               forHTTPHeaderField: $0.key) }
+            return request
+        } else {
+            let error = NSError(domain: Bundle.main.bundleIdentifier ?? "",
+                                code: 0,
+                                userInfo: [NSLocalizedDescriptionKey: "Could not create url request",
+                                    NSLocalizedFailureReasonErrorKey: "Url components are invalid"])
+            throw error
+        }
+    }
+}
